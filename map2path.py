722e42646bb31d78848631e95be3febda30552aa
wyattraich <wyattraich@gmail.com>
2019-09-25 19:21:18 -0600

    fix bug same as last

--- map2path.py
+++ map2path.py
@@ -44,8 +44,12 @@ def pixel_wise(y_true,y_pred):
 
     #zero = tf.constant(0, dtype=tf.float32) # init constant 0, float64
     zero = tf.Variable(0, dtype=tf.float32)
+    #zero.assign(0)
+    zero_64 = tf.Variable(0, dtype=tf.float64)
+    #zero.assign(0)
     #p6 = tf.constant(0.5, dtype=tf.float32)
     p6 = tf.Variable(0.5, dtype=tf.float32)
+    #p6.assign(0.5)
     where = tf.math.greater(A, p6) #get bool tensor of every place that has any green
     indices = tf.where(where) #get indicies of locations with green
     shift = tf.roll(indices,shift=1,axis=0) #shift indices down one
@@ -54,17 +58,20 @@ def pixel_wise(y_true,y_pred):
     #get rid of first row in diff as this row causes false discontinuities after shift
     #one = tf.constant(1, dtype=tf.int64) #init constant 1, int64
     one = tf.Variable(1, dtype=tf.int64)
+    #one.assign(1)
     #one_f = tf.constant(1, dtype=tf.float64)
     one_f = tf.Variable(1, dtype=tf.float64)
+    #one_f.assign(1)
     #two = tf.constant(2,dtype=tf.int32) #init constant 2, int64
     two = tf.Variable(2,dtype=tf.int32)
+    #two.assign(2)
     siz = tf.size(diff_pre) #get length of diff matrix (both cols)
     len = tf.divide(siz,two)
     len_m_one = tf.math.subtract(len,one_f) #subract one from num rows for padding 1 vec
 
     is_empty = tf.equal(tf.size(indices), 0)
     #len_m_one = tf.cond(is_empty, lambda: tf.constant(0,dtype=tf.float64), lambda: len_m_one)
-    len_m_one = tf.cond(is_empty, lambda: tf.Variable(0,dtype=tf.float64), lambda: len_m_one)
+    len_m_one = tf.cond(is_empty, lambda: zero_64, lambda: len_m_one)
 
     one_v1 = tf.ones([1,2],dtype=tf.int64) #init [1,1]
     one_v2 = tf.ones([len,2],dtype=tf.int64)# init [n,2] ones to subtract from vec
@@ -98,15 +105,19 @@ def pixel_wise(y_true,y_pred):
     # return the sum of both discontinuites if it is discontinuous. if it is continuous, return zero loss
     result = tf.cond(d, lambda: zero, lambda: tf.math.add(tf.cast(discont_1, tf.float32),tf.cast(discont_2, tf.float32)))
     #result = tf.cond(is_empty, lambda: tf.constant(10000, dtype=tf.float32), lambda: result)
-    result = tf.cond(is_empty, lambda: tf.Variable(10000, dtype=tf.float32), lambda: result)
+    one_thou = tf.Variable(10000, dtype=tf.float32)
+    #one_thou.assign(1000)
+    result = tf.cond(is_empty, lambda: one_thou, lambda: result)
 
     #L1 loss addition
     abs_sum = K.sum(K.abs(y_true[0,:,:,1] - y_pred[0,:,:,1]))
     abs_sum_32 = tf.cast(abs_sum, dtype=tf.float32)
     #scale_l1 = tf.constant(0.01, dtype=tf.float32)
     scale_l1 = tf.Variable(0.01, dtype=tf.float32)
+    #scale_l1.assign(0.01)
     #scale_discont = tf.constant(0.0001, dtype=tf.float32)
     scale_discont = tf.Variable(0.0001, dtype=tf.float32)
+    #scale_discont.assign(0.0001)
     L1 = tf.multiply(scale_l1,abs_sum_32)
     cust = tf.multiply(scale_discont,result)
 
@@ -304,9 +315,9 @@ class Pix2Pix():
 
                 asdf = pixel_wise(imgs_A,fake_A)
 
-                sess = tf.Session() #initialize tf session
-                with sess.as_default():
-                    print(asdf.eval())
+                #sess = tf.Session() #initialize tf session
+                #with sess.as_default():
+                    #print(asdf.eval())
                 #g_loss_np = pixel_wise_np(imgs_A,fake_A)
 
                 #apply custom loss on generator
